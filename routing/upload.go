b5054b6aa200ea3734273dcc54344700d122e4d5
Wiktor Kania <roller47@wp.pl>
2021-12-12 00:25:01 +0100

    add sending file to storage service

--- routing/upload.go
+++ routing/upload.go
@@ -3,15 +3,72 @@ package routing
 import (
 	"DonTaskMe-backend/internal/helpers"
 	"DonTaskMe-backend/internal/model"
+	"bytes"
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"log"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"path/filepath"
+
 	"github.com/gin-gonic/gin"
 	nano "github.com/matoous/go-nanoid"
 	"go.mongodb.org/mongo-driver/mongo"
-	"net/http"
-	"path/filepath"
 )
 
+const (
+	storageUrl = "http://dontstoreme.vxm.pl/index.php"
+)
+
+type FileResponse struct {
+	Success bool   `json:"success,omitempty"`
+	Msg     string `json:"msg,omitempty"`
+	Path    string `json:"path,omitempty"`
+}
+
+func UploadFile(path string) (FileResponse, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return FileResponse{}, err
+	}
+	defer file.Close()
+
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	part, _ := writer.CreateFormFile("file", filepath.Base(file.Name()))
+	io.Copy(part, file)
+	part2, _ := writer.CreateFormField("key")
+	io.Copy(part2, bytes.NewBufferString("97e5fe51-dcd4-4bdf-ac2e-0d96c990f9fc"))
+	writer.Close()
+
+	req, err := http.NewRequest(http.MethodPost, storageUrl, body)
+	if err != nil {
+		return FileResponse{}, err
+	}
+
+	client := &http.Client{}
+	req.Header.Add("User-Agent", "Mozilla/5.0")
+	req.Header.Add("Content-Type", writer.FormDataContentType())
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return FileResponse{}, err
+	}
+
+	content, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return FileResponse{}, err
+	}
+
+	var response FileResponse
+	err = json.Unmarshal(content, &response)
+	return response, err
+}
+
 func saveFile(c *gin.Context) {
-	token := c.Query("token")
+	token := c.PostForm("token")
 	_, err := helpers.FindUserByToken(token)
 	if err == mongo.ErrNoDocuments {
 		c.AbortWithStatusJSON(http.StatusExpectationFailed, err)
@@ -29,24 +86,38 @@ func saveFile(c *gin.Context) {
 
 	extension := filepath.Ext(file.Filename)
 	UID, _ := nano.Nanoid()
-	cardUID := c.Param("card")
 
 	newFilename := UID + extension
-	storagePath := "files/" + cardUID + "/"
+	storagePath := "uploads/"
+	newPath := storagePath + newFilename
 
+	if err = c.SaveUploadedFile(file, newPath); err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, "Unable to save the file")
+		return
+	}
+
+	res, err := UploadFile(newPath)
+
+	if err != nil {
+		log.Println("err", err)
+		c.AbortWithStatusJSON(http.StatusInternalServerError, "File upload failed")
+		return
+	}
+
+	if !res.Success {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, "File upload failed: "+res.Msg)
+		return
+	}
+
+	cardUID := c.Param("card")
 	fileInfo := model.FileInfo{
-		Filename:    newFilename,
-		StoragePath: storagePath,
+		Filename:    file.Filename,
+		StoragePath: res.Path,
 	}
 
 	err = fileInfo.Save(c, cardUID)
 	if err != nil {
 		c.AbortWithStatusJSON(http.StatusBadRequest, "Unable to save the file's metadata")
-		return
-	}
-
-	if err = c.SaveUploadedFile(file, storagePath+newFilename); err != nil {
-		c.AbortWithStatusJSON(http.StatusInternalServerError, "Unable to save the file")
 		return
 	}
 
